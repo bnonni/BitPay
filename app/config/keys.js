e4e02d27253b63441758eccc4a3a7fb6bb2cc42b
bnonni <bryanwnonni@gmail.com>
2019-11-07 13:25:46 -0500

    commiting local changes

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,4 +1,5 @@
+const pwd = process.env.LocalMongoPassword;
 module.exports = {
-    MongoURI: "mongodb://localhost:27017/bitpay",
+    MongoURI: "mongodb://hu5ky5n0w" + pwd + "localhost:27017/bitpay",
     secretOrKey: "secret"
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express"); //require middleware
+var router = express.Router(); //import router
+const User = require("../models/user"); //require mongoose connection & model
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Encrypter' });
+/* Get home page */
+router.get("/", (req, res, next) => {
+    res.render('index', { title: 'Login' });
+});
+
+router.get("/register", (req, res) => {
+    res.render("game");
+});
+
+router.post("/register", (req, res) => {
+    var username = req.body.log_username;
+    var userpass = req.body.log_password
+
+    var login = {
+        uname: username,
+        pass: userpass
+    }
+    console.log(login);
+
+    User.findOne({ "username": username }, (err, user) => {
+        if (user == null) {
+            console.log(err);
+            res.render("/", { login_fail_msg: "Login failed! Username and password not set. Please set your username and password." });
+        } else if (userpass != user.password) {
+            console.log(user);
+            res.render("login", { login_fail_msg: "Password Invalid! Please try again." });
+        } else if ((!err) && (userpass == user.password)) {
+            res.redirect(302, "/users");
+        }
+    })
 });
 
 module.exports = router;
