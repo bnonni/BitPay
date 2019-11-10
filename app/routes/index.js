01698176948d01eec8c07f8f4c1569f4b529fe7a
bnonni <bryanwnonni@gmail.com>
2019-11-10 18:36:22 -0500

    commiting local changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -35,7 +35,7 @@ router.post('/register', (req, res) => {
                     newUser.save()
                     User.findOne({ 'username': username })
                         .then(usr => {
-                            res.render('users', { register_success_msg: 'Welcome ' + username + '!', title: 'BitPay Developer Assessment', signature: null, username: username });
+                            res.render('users', { register_success_msg: 'Welcome ' + username + '!', title: 'BitPay Developer Assessment', signature: null, username: username, missing_key: 'hidden' });
                         })
                         .catch(err => {
                             res.render('index', { register_fail_msg: 'Registration failed! Please try again.', title: 'BitPay Developer Assessment' });
@@ -49,29 +49,26 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/store', (req, res) => {
-    const begin = /-----BEGIN PUBLIC KEY-----/;
+    const begin = /BEGIN | END/;
     var publickey = req.body.publickey,
         username = req.body.username;
     if (begin.test(publickey)) {
-        publickey = publickey.replace('-----BEGIN PUBLIC KEY-----', '')
-        publickey = publickey.replace('-----END PUBLIC KEY-----', '')
-        kpublickeyey = publickey.split(" ").join("\n");
-        publickey = '-----BEGIN PUBLIC KEY-----\n' + publickey + '\n-----END PUBLIC KEY-----\n';
+        return res.status(400).render('users', { publickey_store_fail: 'Error! Public Key Not Stored. Remove -----BEGIN PUBLIC KEY----- and -----END PUBLIC KEY-----', title: 'BitPay Developer Assessment', signature: null, username: username, missing_key: 'hidden' });
     } else {
         publickey = publickey.split(" ").join("\n");
         publickey = '-----BEGIN PUBLIC KEY-----\n' + publickey + '\n-----END PUBLIC KEY-----\n'
+        console.log(publickey)
     }
     User.findOne({ username: username })
         .then(usr => {
             const id = usr._id;
             User.findOneAndUpdate({ _id: id }, { public_key: publickey, private_key: '' }, { upsert: true }, (err, doc) => {
-                (err ? (console.log(err)) : res.render('users', { publickey_success_msg: 'Public Key Stored.', title: 'BitPay Developer Assessment', signature: null, username: username }));
+                (err ? (console.log(err)) : res.render('users', { publickey_store_success: 'Public Key Stored.', title: 'BitPay Developer Assessment', signature: null, username: username, missing_key: 'hidden' }));
             });
         })
         .catch(err => {
-            res.status(400).render('users', { msg_sign_fail: 'Error! Message Not Signed. Please enter your private key and retry.', title: 'BitPay Developer Assessment', signature: null, username: username });
-            throw err;
-        })
+            res.status(400).render('users', { publickey_store_fail: 'Error! Public Key Not Stored. Ensure proper formatting.', title: 'BitPay Developer Assessment', signature: null, username: username, missing_key: 'hidden' });
+        });
 });
 
 router.post('/generate', (req, res) => {
@@ -81,47 +78,53 @@ router.post('/generate', (req, res) => {
             publicKeyEncoding: { type: 'spki', format: 'pem' },
             privateKeyEncoding: { type: 'pkcs8', format: 'pem' }
         });
-    User.findOne({ 'username': username }).then(usr => {
-        const _id = usr._id;
-        User.findOneAndUpdate({ _id: _id }, { public_key: publicKey, private_key: privateKey }, { upsert: true }, (err, doc) => {
-            (err ? (console.log(err)) : res.render('users', { publickey_success_msg: 'RSA Key Pair Stored.', title: 'BitPay Developer Assessment', signature: null, username: username, }));
+    User.findOne({ username: username })
+        .then(usr => {
+            const _id = usr._id;
+            User.findOneAndUpdate({ _id: _id }, { public_key: publicKey, private_key: privateKey }, { upsert: true }, (err, doc) => {
+                (err ? (console.log(err)) : res.render('users', { publickey_store_success: 'RSA Key Pair Generated & Stored.', title: 'BitPay Developer Assessment', signature: null, username: username, missing_key: 'hidden' }));
+            });
+        })
+        .catch(err => {
+            res.status(400).render('users', { publickey_store_fail: 'Error! RSA Key Pair Stored Not Generated & Stored. Please try again.', title: 'BitPay Developer Assessment', signature: null, username: username, missing_key: 'hidden' });
         });
-    });
-
 });
 
 router.post('/sign', (req, res) => {
+    var private_key, private_key_send, show = false;
     const username = req.body.username,
-        message = req.body.message,
-        sign = crypto.createSign('SHA256');
+        message = req.body.message;
     User.findOne({ username: username })
         .then(usr => {
-            const private_key = usr.private_key,
-                _id = usr._id;
-            if (private_key == '') {
-                User.findOne({ 'username': 'Client' })
-                    .then(usr => {
-                        const private_key = usr.private_key;
-                        sign.update(message, 'utf-8');
-                        sign.end()
-                        const signature = sign.sign(private_key, 'hex');
-                        User.findOneAndUpdate({ _id: _id }, { message: message, signature: signature }, { upsert: true }, (err, doc) => {
-                            res.render('users', { msg_sign_success: 'Message Signed.', title: 'BitPay Developer Assessment', signature: signature, username: username });
-                        });
-                    })
-                    .catch(err => {
-                        res.status(400).render('users', { msg_sign_fail: 'Error! Message Not Signed. Please enter your private key and retry.', title: 'BitPay Developer Assessment', signature: null, username: username });
-                        throw err;
-                    })
-            } else {
+            private_key_send = req.body.privatekey;
+            private_key = usr.private_key;
+            const _id = usr._id;
+            const sign = crypto.createSign('SHA256');
+            if (private_key) {
                 sign.update(message, 'utf-8');
                 sign.end()
                 const signature = sign.sign(private_key, 'hex');
                 User.findOneAndUpdate({ _id: _id }, { message: message, signature: signature }, { upsert: true }, (err, doc) => {
-                    res.render('users', { msg_sign_success: 'Message Signed.', title: 'BitPay Developer Assessment', signature: signature, username: username });
-                    // console.log(doc + ' updated!');
+                    show = true;
+                    res.status(200).render('users', { msg_sign_success: 'Message Signed.', title: 'BitPay Developer Assessment', signature: signature, username: username, missing_key: 'hidden' });
                 });
+            } else if (private_key_send) {
+                private_key_send = '-----BEGIN PRIVATE KEY-----\n' + req.body.privatekey + '\n-----END PRIVATE KEY-----\n';
+                sign.update(message, 'utf-8');
+                sign.end()
+                const signature = sign.sign(private_key_send, 'hex');
+                User.findOneAndUpdate({ _id: _id }, { message: message, signature: signature }, { upsert: true }, (err, doc) => {
+                    show = true;
+                    res.status(200).render('users', { msg_sign_success: 'Message Signed.', title: 'BitPay Developer Assessment', signature: signature, username: username, missing_key: 'show' });
+                });
+            } else {
+                show = true;
+                res.status(400).render('users', { msg_sign_fail: 'Error! Message Not Signed. Missing Private Key. Either generate new keys above or enter private key below.', title: 'BitPay Developer Assessment', signature: null, username: username, missing_key: 'show' });
             }
+        })
+        .catch(err => {
+            var missing = 'hidden' ? show = false : missing = 'show';
+            res.status(400).render('users', { msg_sign_fail: 'Error! Message Not Signed. Please check key formatting & try again.', missing_key: missing, title: 'BitPay Developer Assessment', signature: null, username: username });
         });
 });
 
